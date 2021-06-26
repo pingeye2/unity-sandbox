8a9a2eae2aebad9ffbf5617bb0863ed8aa910b97
Charlie Laver <charlielaver7@gmail.com>
2021-06-26 17:51:42 +0100

    y axis

---
+++ Assets/Scripts/world/yAxisBlocks.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yAxisBlocks : MonoBehaviour
+{
+    // detect when the block gets destroyed
+    // work out the y axis of where the new block will go
+    // if the block is destroyed instantiate a new block in position directly below it
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
