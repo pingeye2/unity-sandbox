f6d741851167fd02b0cfeb70e1478b8b4727733f
Charlie Laver <charlielaver7@gmail.com>
2021-07-03 19:28:29 +0100

    conditional probability

--- Assets/Scripts/world/ProceduralGeneration.cs
+++ Assets/Scripts/world/ProceduralGeneration.cs
@@ -13,7 +13,7 @@ public class ProceduralGeneration : MonoBehaviour
     private List<Vector3> blockPositions = new List<Vector3>();
     private GameObject objectToSpawn;
     private bool loadSceneOnStart = true;
-    private GameObject[] blocks = new GameObject[4];
+    private GameObject[] blocks = new GameObject[20];
     private Vector3 startPos;
 
     //when adding new zone create new arr below and amend selectZone()
@@ -104,34 +104,12 @@ public class ProceduralGeneration : MonoBehaviour
         return Mathf.PerlinNoise(xNoise, zNoise);
     }
 
-    private void selectBlocks()
-    {
-        float randomSpawnNum = Random.Range(0f, 50f);
-
-        if (randomSpawnNum < 0.1f && randomSpawnNum > 0f)
-        {
-            objectToSpawn = blocks[0];
-        }
-        else if (randomSpawnNum < 0.2f && randomSpawnNum > 0.1f)
-        {
-            objectToSpawn = blocks[1];
-        }
-        else if (randomSpawnNum < 0.3f && randomSpawnNum > 0.2f)
-        {
-            objectToSpawn = blocks[2];
-        }
-        else if (randomSpawnNum < 50f && randomSpawnNum > 0.3f)
-        {
-            objectToSpawn = blocks[3];
-        }
-    }
-
     private void zones(GameObject[] zoneArr)
     {
-        blocks[0] = zoneArr[0];
-        blocks[1] = zoneArr[1];
-        blocks[2] = zoneArr[2];
-        blocks[3] = zoneArr[3];
+        for (int i = 0; i < zoneArr.Length; i++)
+        {
+            blocks[i] = zoneArr[i];
+        }
     }
 
     private void selectZone()
@@ -149,6 +127,94 @@ public class ProceduralGeneration : MonoBehaviour
         else if (changeZoneNum > 20 && changeZoneNum < 30)
         {
             zones(zone3Arr);
+        }
+    }
+
+    private void selectBlocks()
+    {
+        float randomSpawnNum = Random.Range(0f, 50f);
+
+        // very rare
+        if (randomSpawnNum < 0.001f && randomSpawnNum > 0f)
+        {
+            objectToSpawn = blocks[0];
+        }
+        // rare
+        else if (randomSpawnNum < 0.01f && randomSpawnNum > 0.001f)
+        {
+            objectToSpawn = blocks[1];
+        }
+        else if (randomSpawnNum < 0.02f && randomSpawnNum > 0.01f)
+        {
+            objectToSpawn = blocks[2];
+        }
+        // uncommon
+        else if (randomSpawnNum < 0.1f && randomSpawnNum > 0.02f)
+        {
+            objectToSpawn = blocks[3];
+        }
+        else if (randomSpawnNum < 0.2f && randomSpawnNum > 0.1f)
+        {
+            objectToSpawn = blocks[4];
+        }
+        else if (randomSpawnNum < 0.3f && randomSpawnNum > 0.2f)
+        {
+            objectToSpawn = blocks[5];
+        }
+        else if (randomSpawnNum < 0.4f && randomSpawnNum > 0.3f)
+        {
+            objectToSpawn = blocks[6];
+        }
+        else if (randomSpawnNum < 0.5f && randomSpawnNum > 0.4f)
+        {
+            objectToSpawn = blocks[7];
+        }
+        // common
+        else if (randomSpawnNum < 1f && randomSpawnNum > 0.5f)
+        {
+            objectToSpawn = blocks[8];
+        }
+        else if (randomSpawnNum < 2f && randomSpawnNum > 1f)
+        {
+            objectToSpawn = blocks[9];
+        }
+        else if (randomSpawnNum < 3f && randomSpawnNum > 2f)
+        {
+            objectToSpawn = blocks[10];
+        }
+        else if (randomSpawnNum < 4f && randomSpawnNum > 3f)
+        {
+            objectToSpawn = blocks[12];
+        }
+        else if (randomSpawnNum < 5f && randomSpawnNum > 4f)
+        {
+            objectToSpawn = blocks[13];
+        }
+        // very common
+        else if (randomSpawnNum < 10f && randomSpawnNum > 5f)
+        {
+            objectToSpawn = blocks[14];
+        }
+        else if (randomSpawnNum < 15f && randomSpawnNum > 10f)
+        {
+            objectToSpawn = blocks[15];
+        }
+        else if (randomSpawnNum < 20f && randomSpawnNum > 15f)
+        {
+            objectToSpawn = blocks[16];
+        }
+        // very very common
+        else if (randomSpawnNum < 30f && randomSpawnNum > 20f)
+        {
+            objectToSpawn = blocks[17];
+        }
+        else if (randomSpawnNum < 40f && randomSpawnNum > 30f)
+        {
+            objectToSpawn = blocks[18];
+        }
+        else if (randomSpawnNum < 50f && randomSpawnNum > 40f)
+        {
+            objectToSpawn = blocks[19];
         }
     }
 }
